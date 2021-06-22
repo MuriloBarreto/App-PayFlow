e40422a38585061e5c9a4cba5f799220cc2249f9
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-06-22 16:23:05 -0300

    inserindo temas de estilização e fazendo página de splash e login

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HOmePage extends StatelessWidget {
+  const HOmePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("HomePage"),),
+    );
+  }
+}
